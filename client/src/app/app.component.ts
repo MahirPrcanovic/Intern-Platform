8c7859b314651a3b8526297c2d6ef5fc7afd439c
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-02 16:24:03 +0100

    initial: aos package added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
   title = 'client';
   constructor(private http: HttpClient) {}
   ngOnInit() {
+    AOS.init();
     this.http
       .get('https://localhost:7213/WeatherForecast')
       .subscribe((data) => console.log(data));

e51847de37157f423799a614365ba131b76d5dc3
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-02 11:49:33 +0100

    initial: server files + client bootrap added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get('https://localhost:7213/WeatherForecast')
+      .subscribe((data) => console.log(data));
+  }
 }

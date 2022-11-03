735e103446a57b440e288a0a095054313a468bc9
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-03 10:41:39 +0100

    feat: header, hero and selection section finished

---
+++ client/src/app/components/landing-page/selection-section/selection-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selection-section',
+  templateUrl: './selection-section.component.html',
+  styleUrls: ['./selection-section.component.css']
+})
+export class SelectionSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

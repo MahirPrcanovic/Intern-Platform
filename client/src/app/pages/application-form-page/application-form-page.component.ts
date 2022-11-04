baa17980d46c8d151e6176170bbd041cdb1f3bfb
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-04 15:29:16 +0100

    feat: backend connected with frontend everything working, services needed in back

--- client/src/app/pages/application-form-page/application-form-page.component.ts
+++ client/src/app/pages/application-form-page/application-form-page.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApplicationsService } from 'src/app/services/applications.service';
 
 @Component({
   selector: 'app-application-form-page',
@@ -8,14 +10,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./application-form-page.component.css'],
 })
 export class ApplicationFormPageComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private applicationService: ApplicationsService
+  ) {}
   formValid = true;
   submitted = false;
   ngOnInit(): void {}
   onSubmit(f: NgForm) {
     console.log(f);
-    this.submitted = f.submitted;
-    this.formValid = f.form.valid;
+    this.applicationService.postData(f.form.value).subscribe((response) => {
+      this.submitted = f.submitted;
+      this.formValid = f.form.valid;
+
+      console.log(response);
+    });
   }
   resetForm() {
     this.formValid = true;
---
+++ server/InternshipPlatformAPI/InternshipPlatformAPI/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using InternshipPlatformAPI.Dtos;
+using InternshipPlatformAPI.Models;
+
+namespace InternshipPlatformAPI.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ApplicationFormDto, Application>();
+        }
+    }
+}

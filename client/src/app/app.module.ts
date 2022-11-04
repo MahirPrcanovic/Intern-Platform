a9764849765f025853774b1ef8ad1af81c6b640c
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-04 09:56:13 +0100

    fix: few minor bugs in merging

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,15 +11,27 @@ import { HeroSectionComponent } from './components/landing-page/hero-section/her
 import { SelectionSectionComponent } from './components/landing-page/selection-section/selection-section.component';
 import { SubscribeSectionComponent } from './components/landing-page/subscribe-section/subscribe-section.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
-
+// import { DashboardHeaderComponent } from './components/applications-page/header/dashboard-header/dashboard-header.component';
 import { DashboardHeaderComponent } from './components/applications-page/header/dashboard-header/dashboard-header.component';
 import { ApplicationsPageComponent } from './pages/applications-page/applications-page/applications-page.component';
 import { ApplicationHeroComponent } from './components/applications-page/hero-section/application-hero/application-hero.component';
 import { ApplicationFormPageComponent } from './pages/application-form-page/application-form-page.component';
 
-import { ApplicationFormPageComponent } from './pages/application-form-page/application-form-page.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginComponent,LandingPageComponent, HeroSectionComponent, SelectionSectionComponent, SubscribeSectionComponent, FooterComponent, ApplicationFormPageComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    LandingPageComponent,
+    HeroSectionComponent,
+    SelectionSectionComponent,
+    SubscribeSectionComponent,
+    FooterComponent,
+    ApplicationFormPageComponent,
+    DashboardHeaderComponent,
+    ApplicationsPageComponent,
+    ApplicationHeroComponent,
+  ],
 
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
